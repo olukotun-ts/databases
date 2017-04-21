686cb46a8cdc8f3493f3526b1a5a0cf8933e27d9
Sam Olukotun <otsam88@gmail.com>
2017-04-21 16:45:48 -0700

    Working schema.

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,33 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER AUTO_INCREMENT,
+  message VARCHAR(150),
+  user_id INTEGER,
+  room_id INTEGER,
+  createdAt TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
 
-
+ALTER TABLE messages ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE messages ADD CONSTRAINT FOREIGN KEY (room_id) REFERENCES rooms (id);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
